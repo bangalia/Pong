27d0b2d3cebe9cbc9c0c2efb72d203e7ff1fb495
Johnny Quinn <jftq97@gmail.com>
2021-10-06 14:24:19 -0400

    Updated app.js for server to run on port 3000, install

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to run server 
+
+In terminal within the main directory: 
+`npm run dev`
--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require("express")
 const app = express()
 const http = require('http')
 
+const port = 3000;
+
 //Socket.io
-const io = require('socket.io')(server)
+// const io = require('socket.io')(server)
 
 app.get('/', (req, res) => {
-    res.sendFile(_dirname + '/index.html')
+    res.sendFile(__dirname + '/static/index.html')
 })
+
+app.listen(port, () => console.log(`Pong app listening on port ${port}!`));
