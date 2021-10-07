15652542ed2c9463fee06839166290d6ed369b52
bangalia <anitabangali@yahoo.com>
2021-10-06 23:59:09 -0500

    started client side

---
+++ static/client.js
@@ -0,0 +1,20 @@
+// var UP    = 'UP',
+//     LEFT  = 'LEFT',
+//     DOWN  = 'DOWN',
+//     RIGHT = 'RIGHT';
+
+// socket.connect();
+
+// socket.on('message', function(msg) {
+//     if(msg == 'UP') {
+//         player.moveUP();
+//     } else if(msg == 'LEFT') {
+//         player.moveLEFT();
+//     } else if(msg == 'DOWN') {
+//         player.moveDOWN();
+//     } else if(msg == 'RIGHT') {
+//         player.moveRIGHT();
+//     } else {
+
+//     }
+// });
