c696d54f20cd1c3a4b9d63165cb1b91e455e261f
Johnny Quinn <jftq97@gmail.com>
2021-10-07 18:24:51 -0400

    made color elements headings in html, added some styling to css, and set current_color var in js

--- static/client.js
+++ static/client.js
@@ -5,13 +5,16 @@ const blue = document.getElementById('blue')
 const current_color = 'red';
 
 red.addEventListener('click', function(){
-    color_box.style.backgroundColor = "red"
+    color_box.style.backgroundColor = "red";
+    current_color = "red"
 })
 
 green.addEventListener('click', function(){
     color_box.style.backgroundColor = "green"
+    current_color = "green"
 })
 
 blue.addEventListener('click', function(){
     color_box.style.backgroundColor = "blue"
+    current_color = "blue"
 })
