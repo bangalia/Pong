e39878ac37179b513d09d8b90965b1971c3c1a88
Johnny Quinn <jftq97@gmail.com>
2021-10-07 18:18:13 -0400

    wrote frontend code for color box

--- static/client.js
+++ static/client.js
@@ -1,4 +1,17 @@
-$(document).ready(() => {
-    const socket = io.connect();
-    let currentUser; 
+const color_box = document.getElementById('color-box')
+const red = document.getElementById('red')
+const green = document.getElementById('green')
+const blue = document.getElementById('blue')
+const current_color = 'red';
+
+red.addEventListener('click', function(){
+    color_box.style.backgroundColor = "red"
+})
+
+green.addEventListener('click', function(){
+    color_box.style.backgroundColor = "green"
+})
+
+blue.addEventListener('click', function(){
+    color_box.style.backgroundColor = "blue"
 })
