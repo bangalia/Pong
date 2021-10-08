348810cebcbd971590fd80ac70bde747845de466
Johnny Quinn <jftq97@gmail.com>
2021-10-07 20:18:52 -0400

    added colors array and color pagination routes in app.js, updated some stuff in html and css, and added pagination code in js

--- static/client.js
+++ static/client.js
@@ -1,20 +1,73 @@
 const color_box = document.getElementById('color-box')
-const red = document.getElementById('red')
-const green = document.getElementById('green')
-const blue = document.getElementById('blue')
+
+const color1 = document.getElementById('color1')
+color1.style.color = "red";
+const color2 = document.getElementById('color2')
+color2.style.color = "green";
+const color3 = document.getElementById('color3')
+color3.style.color = "blue";
 const current_color = 'red';
 
-red.addEventListener('click', function(){
-    color_box.style.backgroundColor = "red";
-    current_color = "red"
+const page1 = document.getElementById('page1')
+const page2 = document.getElementById('page2')
+const page3 = document.getElementById('page3')
+const current_page = 1;
+
+color1.addEventListener('click', function(){
+    colorText = color1.innerText
+    color_box.style.backgroundColor = colorText;
+    current_color = colorText;
 })
 
-green.addEventListener('click', function(){
-    color_box.style.backgroundColor = "green"
-    current_color = "green"
+color2.addEventListener('click', function(){
+    colorText = color2.innerText
+    color_box.style.backgroundColor = colorText;
+    current_color = colorText;
 })
 
-blue.addEventListener('click', function(){
-    color_box.style.backgroundColor = "blue"
-    current_color = "blue"
+color3.addEventListener('click', function(){
+    colorText = color3.innerText
+    color_box.style.backgroundColor = colorText;
+    current_color = colorText;
 })
+
+page1.addEventListener('click', function(){
+    const colors = httpGet('http://localhost:3000/colors1')
+    console.log(colors)
+    color1.innerText = colors[0];
+    color1.style.color = colors[0];
+    color2.innerText = colors[1];
+    color2.style.color = colors[1];
+    color3.innerText = colors[2];
+    color3.style.color = colors[2];
+})
+
+page2.addEventListener('click', function(){
+    const colors = httpGet('http://localhost:3000/colors2')
+    console.log(colors)
+    color1.innerText = colors[0];
+    color1.style.color = colors[0];
+    color2.innerText = colors[1];
+    color2.style.color = colors[1];
+    color3.innerText = colors[2];
+    color3.style.color = colors[2];
+})
+
+page3.addEventListener('click', function(){
+    const colors = httpGet('http://localhost:3000/colors3')
+    console.log(colors)
+    color1.innerText = colors[0];
+    color1.style.color = colors[0];
+    color2.innerText = colors[1];
+    color2.style.color = colors[1];
+    color3.innerText = colors[2];
+    color3.style.color = colors[2];
+})
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.responseText);
+}
